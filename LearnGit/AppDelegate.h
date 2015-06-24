0dfe7fe1501ee5d8f52276fdb459b0adc508b2a7
NoWhere <zhudengdi@qq.com>
2015-06-24 09:43:44 +0800

    Initial Commit

---
+++ LearnGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnGit
+//
+//  Created by lanouhn on 15/6/24.
+//  Copyright (c) 2015年 lanou3G.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
